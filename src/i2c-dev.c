014d2809dc1de58b4644c468fcdd9e69af54ef12
vitalogy <vitalogy_github@milaw.biz>
2015-11-29 13:41:32 +0100

    src/i2c-dev: inline via extern

--- src/i2c-dev.c
+++ src/i2c-dev.c
@@ -9,65 +9,22 @@
 #ifndef _WIRING_X_I2C_DEV_H_
 #define _WIRING_X_I2C_DEV_H_
 
-#include <stdlib.h>
 #include <sys/ioctl.h>
 #include <linux/types.h>
 #include <linux/i2c.h>
 #include <linux/i2c-dev.h>
 
-inline __s32 i2c_smbus_access(int fd, char rw, int cmd, int size, union i2c_smbus_data *data) {
-	struct i2c_smbus_ioctl_data args;
+#define INLINE
+#include "i2c-dev.h"
 
-	args.read_write = rw;
-	args.command = cmd;
-	args.size = size;
-	args.data = data;
 
-	return ioctl(fd, I2C_SMBUS, &args);
-}
-
-inline __s32 i2c_smbus_read_byte(int fd) {
-	union i2c_smbus_data data;
-	if(i2c_smbus_access(fd, I2C_SMBUS_READ, 0, I2C_SMBUS_BYTE, &data) <= 0) {
-		return -1;
-	} else {
-		return 0x0FF & data.byte;
-	}
-}
-
-inline __s32 i2c_smbus_write_byte(int fd, int value) {
-	return i2c_smbus_access(fd, I2C_SMBUS_WRITE, value, I2C_SMBUS_BYTE, NULL);
-}
-
-inline __s32 i2c_smbus_read_byte_data(int fd, int cmd) {
-	union i2c_smbus_data data;
-	if(i2c_smbus_access(fd, I2C_SMBUS_READ, cmd, I2C_SMBUS_BYTE_DATA, &data) <= 0) {
-		return -1;
-	} else {
-		return 0x0FF & data.byte;
-	}
-}
-
-inline __s32 i2c_smbus_write_byte_data(int fd, int cmd, int value) {
-	union i2c_smbus_data data;
-	data.byte = value;
-	return i2c_smbus_access(fd, I2C_SMBUS_WRITE, cmd, I2C_SMBUS_BYTE_DATA, &data);
-}
-
-inline __s32 i2c_smbus_read_word_data(int fd, int cmd) {
-	union i2c_smbus_data data;
-	if(i2c_smbus_access(fd, I2C_SMBUS_READ, cmd, I2C_SMBUS_WORD_DATA, &data) <= 0) {
-		return -1;
-	} else {
-		return 0x0FFFF & data.word;
-	}
-}
-
-inline __s32 i2c_smbus_write_word_data(int fd, int cmd, __u16 value) {
-	union i2c_smbus_data data;
-	data.word = value;
-
-	return i2c_smbus_access(fd, I2C_SMBUS_WRITE, cmd, I2C_SMBUS_WORD_DATA, &data);
-}
+extern __s32 i2c_smbus_access(int fd, char rw, int cmd, int size, union i2c_smbus_data *data);
+extern __s32 i2c_smbus_read_byte(int fd);
+extern __s32 i2c_smbus_write_byte(int fd, int value);
+extern __s32 i2c_smbus_read_byte_data(int fd, int cmd);
+extern __s32 i2c_smbus_write_byte_data(int fd, int cmd, int value);
+extern __s32 i2c_smbus_read_word_data(int fd, int cmd);
+extern __s32 i2c_smbus_write_word_data(int fd, int cmd, __u16 value);
 
 #endif
+
